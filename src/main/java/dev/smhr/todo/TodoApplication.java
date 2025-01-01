34dffa822d1454bc90fb31b9e17a08c3056c2d1c
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-31 23:10:08 -0600

    Remove unsused import

--- src/main/java/dev/smhr/todo/TodoApplication.java
+++ src/main/java/dev/smhr/todo/TodoApplication.java
@@ -1,6 +1,5 @@
 package dev.smhr.todo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
