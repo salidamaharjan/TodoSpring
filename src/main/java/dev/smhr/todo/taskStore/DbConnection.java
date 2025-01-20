ebcc593e51508c53bed962b99659055d6a8463d2
Salida Maharjan <salidamaharjan@outlook.com>
2025-01-19 20:29:16 -0600

    Create spring bean for Db connection

--- src/main/java/dev/smhr/todo/taskStore/DbConnection.java
+++ src/main/java/dev/smhr/todo/taskStore/DbConnection.java
@@ -1,12 +1,20 @@
 package dev.smhr.todo.taskStore;
 
+import org.springframework.stereotype.Repository;
+
 import java.sql.*;
 
+@Repository
 public class DbConnection {
     private final String url = "jdbc:postgresql://localhost:5432/";
     private final String db = "my_todo";
     private final String user = "myuser";
     private final String password = "secret";
+
+    public Connection getConnection()throws SQLException{
+        Connection connection = DriverManager.getConnection(url + db, user, password);
+        return connection;
+    }
 
     public void createTable() throws SQLException {
         String createTableSQL = """ 
@@ -15,7 +23,7 @@ public class DbConnection {
                     task_name TEXT, 
                     completed BOOLEAN )
                 """;
-        try (Connection connection = DriverManager.getConnection(url + db, user, password);
+        try (Connection connection = getConnection();
              Statement statement = connection.createStatement()) {
             statement.execute(createTableSQL);
             System.out.println("Table 'tasks' created successfully.");
