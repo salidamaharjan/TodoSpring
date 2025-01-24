1af50e0a2cd0a6efdc167abf85c8c665484b47d1
Salida Maharjan <salidamaharjan@outlook.com>
2025-01-24 09:59:01 -0600

    Create connection to get all the task

--- src/main/java/dev/smhr/todo/taskStore/DbTaskStore.java
+++ src/main/java/dev/smhr/todo/taskStore/DbTaskStore.java
@@ -4,6 +4,7 @@ import dev.smhr.todo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +26,7 @@ public class DbTaskStore implements TaskStoreInterface{
     @Override
     public List<Task> getTask() {
         List<Task> taskList = new ArrayList<>();
-        try {
+        try(Connection connection = connectDB.getConnection()) {
            return allTasks.getAllTasks();
         }catch (SQLException e) {
             e.printStackTrace();
